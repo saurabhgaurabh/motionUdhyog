d4f48406e83503284ed41913a1c1eed69123ff08
saurabh <developersaurabhh@gmail.com>
2025-04-30 01:55:55 +0530

    create index.js or app.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Sample route
+// app.get('/', (req, res) => {
+//   res.json({ message: 'Welcome to the API' });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
