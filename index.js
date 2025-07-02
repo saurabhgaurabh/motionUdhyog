26b52c92046ee29307672b91a24ab258a3de8312
Saurabh Kumar <patiramproduction@gmail.com>
2025-07-02 21:42:34 +0530

    change index language

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(approute)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Bhai tu kuch galat kar raha hai. (Bad Request...)' });
+  res.status(404).json({ message: 'Bhai tu kuch galat kar raha hai URL check kr apna phle. (Bad Request...)' });
 })
 app.listen(PORT, () => {
   console.log(`Server has been activated via port no. ${PORT}`);
